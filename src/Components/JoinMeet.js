deb19f28b1af2dee06c6c2c0be1969c3a80ddd59
Ravi-Sinhmar <engineerfuture82@gmail.com>
2024-10-15 20:36:02 +0530

    all set

--- src/Components/JoinMeet.js
+++ src/Components/JoinMeet.js
@@ -205,6 +205,16 @@ if(adminSocketStatus){
   const adminMessageListener =async (event)=>{
     const data = JSON.parse(event.data);
     // if Someone Reset or Refresh or Firsttime going on link
+
+    if(callStatus === "off"){
+  adminSocket.send(JSON.stringify({ ...wsMessage,type:"off",content: null}));
+    };
+    if(data.type === "off"){
+      disconnect();
+      navigate("/");
+    }
+
+
  if (data.type === "userOn" || data.type === "askingOffer") {
   const offer = await createOffer();
   setFinalOffer(offer);
@@ -241,6 +251,14 @@ if(userSocketStatus && joined){
   };
   const userMessageListener = async(event)=>{
     const data = JSON.parse(event.data);
+
+    if(callStatus === "off"){
+      userSocket.send(JSON.stringify({ ...wsMessage,type:"off",content: null}));
+        };
+        if(data.type === "off"){
+          disconnect();
+          navigate("/");
+        }
     // If admin Reset or refresh
     if (data.type === "adminOn") {
     userSocket.send(JSON.stringify({ ...wsMessage,type:"askingOffer"}));
@@ -265,7 +283,7 @@ return () => {
   userSocket.removeEventListener("message", userMessageListener);
 };
 }
-  },[adminSocketStatus,userSocketStatus,adminCon,adminSocket,userSocket,userName,joined,fullName,createAnswer,createOffer,setRemoteAnswer]);
+  },[adminSocketStatus,userSocketStatus,adminCon,adminSocket,userSocket,userName,joined,fullName,createAnswer,createOffer,setRemoteAnswer,disconnect,callStatus,navigate]);
 
   const handleNeg = useCallback(async () => {
     console.log("nego need");
@@ -325,7 +343,7 @@ return () => {
    setTimeout(() => {
    disconnect();
     navigate("/");
-   }, 2000);
+   }, 1000);
   };
 
   const handleMore = useCallback(async () => {
@@ -345,7 +363,6 @@ return () => {
               playsInline
               className="absolute right-3 top-3 rounded-md  object-cover h-24 w-16 ring-1 ring-black"
             ></video>
-         {user && !joined  ? <button className="z-20 w-fit px-6 text-sm font-[400] flex text-center items-center justify-center py-2 rounded-full bg-blm ring-1 ring-blt shadow-md shadow-blt absolute h-fit right-1/2 translate-x-1/2 bottom-10" onClick={handleUserJoin}>Join</button> : null } 
             <div className=" flex flex-col justify-center items-center h-full">
               <video
                 ref={remoteVideoRef}
@@ -433,24 +450,7 @@ return () => {
                   </svg>
                   Stop
                 </button>
-                <button
-                  onClick={cutCall}
-                  className="flex flex-col text-sm items-center justify-center gap-1"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    className="size-8 p-1 bg-ble text-blm rounded-full"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M15.22 3.22a.75.75 0 0 1 1.06 0L18 4.94l1.72-1.72a.75.75 0 1 1 1.06 1.06L19.06 6l1.72 1.72a.75.75 0 0 1-1.06 1.06L18 7.06l-1.72 1.72a.75.75 0 1 1-1.06-1.06L16.94 6l-1.72-1.72a.75.75 0 0 1 0-1.06ZM1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z"
-                      clip-rule="evenodd"
-                    />
-                  </svg>
-                  Disconnect
-                </button>
+                {user ? <button className="z-20 w-fit px-6 text-sm font-[400] flex text-center items-center justify-center py-2 rounded-full bg-blm ring-1 ring-blt shadow-md shadow-blt h-fit" onClick={!joined && !admin ? handleUserJoin : cutCall}>{!joined ? "Join" : "Disconnect"}</button> : null } 
                 <button
                   onClick={toggleRemoteAudio}
                   className="flex flex-col text-sm items-center justify-center gap-1"
