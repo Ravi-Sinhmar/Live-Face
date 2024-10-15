3eeea3e9ef3f1472b2dcc0e6e24e90edf21d3769
Ravi-Sinhmar <engineerfuture82@gmail.com>
2024-10-15 13:33:52 +0530

    friend back to noraml

--- src/Contexts/Friend.jsx
+++ src/Contexts/Friend.jsx
@@ -7,16 +7,13 @@ const MyContext = React.createContext(null);
 function FriendProvider(props){
   const [friend,setFriend] = useState(null);
   const [adminCon,setAdminCon] = useState(null);
-  const [userName, setUserName] = useState(null);
-  const [fullName, setFullName] = useState(null);
-  const [joined, setJoined] = useState(false);
-  const [live, setLive] = useState(false);
     return(
-        <MyContext.Provider value={{friend,setFriend,adminCon,setAdminCon,userName,setUserName,fullName,setFullName,joined,setJoined,live,setLive}}>
+        <MyContext.Provider value={{friend,setFriend,adminCon,setAdminCon}}>
             {props.children}
         </MyContext.Provider>
     )
 }
+
 export default FriendProvider;
 
 
