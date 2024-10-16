36311413836c145a6b135919e3bfca17c781832a
Ravi-Sinhmar <engineerfuture82@gmail.com>
2024-10-17 00:04:56 +0530

    copy the both file st and con to keep the code safe

---
+++ src/Components/Connecting copy.js
@@ -0,0 +1,125 @@
+import React , {useCallback, useEffect, useRef, useState} from "react";
+import { useFriend } from "./../Contexts/Friend";
+import Loader from "./Loader";
+
+function Setting({onContinue}) {
+    const audioInputEl = useRef(null);
+    const audioOutputEl =useRef(null);
+    const videoInputEl =useRef(null);
+    const [isLoading,setIsLoading] = useState(false);
+    const {setCons,setAudioOutput,setSetting} = useFriend();
+    const getStream = useCallback(async () => {
+        try{
+          const permission =  await navigator.mediaDevices.getUserMedia({video:true,audio:true});
+          if(permission){
+            setIsLoading(true);
+          }
+      }catch(err){
+          console.log(err)
+      }},[]);
+      useEffect(()=>{
+          getStream();
+      },[getStream]);
+    const getDevices = useCallback(async()=>{
+        if(isLoading){
+            try{
+           
+                const devices = await navigator.mediaDevices.enumerateDevices();
+                if(devices){
+                    setIsLoading(false);
+                 }
+                setCons({video:true,audio:true});
+                console.log(devices);
+                devices.forEach(d=>{
+                    const option = document.createElement('option') //create the option tag
+                    option.value = d.deviceId
+                    option.text = d.label
+                    //add the option tag we just created to the right select
+                    if(d.kind === "audioinput"){
+                        audioInputEl.current.appendChild(option)    
+                    }else if(d.kind === "audiooutput"){
+                        audioOutputEl.current.appendChild(option)    
+                    }else if(d.kind === "videoinput"){
+                        videoInputEl.current.appendChild(option)    
+                    }
+                })
+            }catch(err){
+                console.log(err);
+            }
+        }
+    },[setCons,isLoading]);
+    useEffect(()=>{
+        getDevices();
+    },[getDevices])
+    const changeAudioInput = async(e)=>{
+        //changed audio input!!!
+        const deviceId = e.target.value;
+        console.log("changed audio input");
+        const newConstraints = {
+            audio: {deviceId: {exact: deviceId}},
+            video: true,
+        };
+        setCons(newConstraints);
+
+    }
+    const changeAudioOutput = async(e)=>{
+       
+        console.log(e.target.value);
+      await  setAudioOutput(e.target.value)
+        console.log("Changed audio device!")
+    }
+    
+    const changeVideo = async(e)=>{
+        //changed video input!!!
+        const deviceId = e.target.value;
+        console.log("changed Vid input");
+       
+        const newConstraints = {
+            audio: true,
+            video: {deviceId: {exact: deviceId}},
+        }
+        setCons(newConstraints);
+    }
+    
+    return(
+    <React.Fragment>
+    { isLoading ? <Loader className="bg-blf">It might take 2 minutes on first time load , so please have paticence</Loader> : null}
+    <div className="flex  justify-center bg-blm  items-center w-svw h-svh">
+    <div className="w-svw h-svh bg-blm  px-4 flex flex-col  justify-around items-center sm:w-10/12  md:w-3/5 lg:w-2/5 md:aspect-square">
+        <div className="flex flex-col items-center w-full gap-8">
+        <button onClick={onContinue}  className="flex  items-center w-full  rounded-md  ">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="h-8 w-8 text-blm  p-2 font-[500] bg-blf  rounded-full">
+      <path stroke-linecap="round" stroke-linejoin="round" d="m18.75 4.5-7.5 7.5 7.5 7.5m-6-15L5.25 12l7.5 7.5" />
+    </svg>
+            
+            </button>
+       <div className="flex flex-col gap-3 w-full justify-between ">
+         <label className=" font-[500] text-blf">Audio Input(Mic)</label>
+         <select onChange={changeAudioInput} ref={audioInputEl} className=" text-blt px-2  ring-1 ring-gray-500 bg-blg rounded-md w-full text-sm  py-3 "  id="audio-input">
+          
+         </select>
+       </div>
+       {/* Sound Output */}
+       <div className="flex flex-col gap-3 w-full justify-between ">
+         <label className="text-blf font-[500] ">Sound Output(Speaker)</label>
+         <select onChange={changeAudioOutput} ref={audioOutputEl} className=" text-blt px-2  ring-1 ring-gray-500 bg-blg rounded-md w-full text-sm  py-3 "  id="audio-input">
+          
+         </select>
+       </div>
+ 
+       {/* Camera Type */}
+       <div className="flex flex-col gap-3 w-full justify-between ">
+         <label className="text-blf font-[500]">Camera Type</label>
+         <select onChange={changeVideo} ref={videoInputEl} className=" text-blt px-2  ring-1 ring-gray-500 bg-blg rounded-md w-full text-sm  py-3 "  id="audio-input">
+         </select>
+       </div>
+     
+     </div>
+     <button onClick={onContinue} className="bg-blf text-blm shadow-sm shadow-black w-full py-2 rounded-full text-lg">Continue</button>
+    </div>
+    </div>
+    </React.Fragment>
+    )
+}
+
+export default Setting;
---
+++ src/Components/StartMeet copy.js
@@ -0,0 +1,120 @@
+import React, { useState, useCallback ,useRef } from "react";
+import { useFriend } from "./../Contexts/Friend";
+import Loader from "./Loader";
+import { useNavigate } from 'react-router-dom';
+import { RandomString } from "../JavaScriptFun/RandomString";
+function StartMeet() {
+ const [isClick, setIsClick] = useState(false);
+ const [adminName, setAdminName] = useState('');
+ const [isLoading,setIsLoading] = useState(false);
+ const [isLink,setIsLink] = useState(null);
+ const [smallLink,setSmallLink] = useState(null);
+
+ const navigate = useNavigate();
+ const {setAdminCon } = useFriend();
+ const linkRef = useRef(null);
+
+// click on start instant meeting
+const startMeet = useCallback(()=>{
+  console.log("StartMeet Click");
+  setIsClick(true);
+},[]);
+
+const handleInputChange = (event) => {
+  setAdminName(event.target.value);
+  setAdminCon(event.target.value.toLowerCase().replace(/\s+/g, ""));
+};
+
+const saveMeet = useCallback(()=>{
+  setIsLoading(true);
+  console.log("saveMeetCallback");
+  const meetId = RandomString(6);
+    console.log("fetch request sent",adminName,meetId);
+    const content = {adminName:adminName,meetingId:meetId}
+    fetch(`https://facesyncbackend.onrender.com/saveMeet`, {
+      method: "POST",
+      credentials: 'include',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(content)
+    }).then(data=>data.json()).then((data)=>{
+      setIsLoading(false);
+if(data.status === 'success'){
+  const cleanName = adminName.toLowerCase().replace(/\s+/g, "");
+  setSmallLink(`/meeting?adminName=${cleanName}&meetingId=${meetId}`)
+  setIsLink(`https://live-face.vercel.app/meeting?adminName=${cleanName}&meetingId=${meetId}`);
+ setIsLoading(false)
+}
+    }).catch(err=>{
+      console.log(err);
+    });
+},[adminName]);
+
+  const copyToClipboard = () => {
+    linkRef.current.select();
+    document.execCommand('copy');
+    alert('Copy Link');
+    navigate(smallLink);
+  };
+
+
+
+  return (
+    <React.Fragment>
+{ isLoading ? <Loader className="bg-blf">It might take 2 minutes on first time load , so please have paticence</Loader> : null}
+
+    <div className=" font-rob flex flex-col h-svh w-full items-center justify-center gap-2 pb-9">
+    
+      <div className="flex flex-col  items-center gap-4">
+        <img
+          className="w-40 aspect-square "
+          src="/images/welcome.png"
+          alt="Welcome"
+        />
+        <div className="flex flex-col items-center justify-center gap-4">
+          <h1 className="text-3xl font-[500] text-blf">Face Sync</h1>
+          <p className="text-gray-500 font-[400] text-center w-10/12">{!isClick ? "Connect, Laugh, and Create moments. Anytime, Anywhere with Love😚": isLink ? "Copy and Share the link with your friend :" :"Enter name, Generate link, Share Link with friend and roast him/her😎" }</p>
+        </div>
+      </div>
+     
+      {!isClick  && (
+        <button onClick={startMeet} className="bg-blf text-white rounded-full py-2 w-4/5 font-[500] text-lg mt-6">Start Instant Meeting</button>
+      )}
+
+{isLink !== null ?  <div className="px-2 py-1 rounded-lg gap-2  flex flex-col items-center justify-center ">
+      <h3 className="text-lg text-blm px-2 rounded-md font-[500]">Copy & Share</h3>
+      <div className="flex items-center justify-center gap-2">
+      <input className="bg-blm w-11/12 px-2 py-2 text-sm rounded-md ring-1 "
+        ref={linkRef}
+        type="text"
+        value={isLink}
+        readOnly
+        style={{
+          marginRight: '10px',
+          whiteSpace: 'nowrap',
+          overflow: 'hidden',
+          textOverflow: 'ellipsis',
+        }}
+      />
+      <button className="bg-blf  px-4 py-2 text-sm rounded-md text-blm" onClick={copyToClipboard}>Copy</button>
+      </div>
+ 
+
+    </div> : null}
+        
+       {isClick && !isLink ? (
+        <div className="w-full flex flex-col justify-center items-center gap-4 pb-10 ">
+         <input value={adminName}
+         onChange={handleInputChange} className=" border-[1px] border-blf w-4/5 py-2 px-3 bg-gray-100 rounded-full mt-8" placeholder="Your name please" type="text" />
+        <button onClick={saveMeet} className="bg-blf text-white rounded-full py-2 w-4/5 font-[500] text-lg">Generate Link</button>
+        </div>
+      ):null}
+    </div>
+    </React.Fragment>
+  );
+}
+export default StartMeet;
+
+ 
+   
