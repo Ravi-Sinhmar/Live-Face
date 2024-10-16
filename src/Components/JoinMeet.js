839a19cf0ad28a78e39c0e9f8588f98c05d9d095
Ravi-Sinhmar <engineerfuture82@gmail.com>
2024-10-16 17:33:52 +0530

    all done

--- src/Components/JoinMeet.js
+++ src/Components/JoinMeet.js
@@ -135,12 +135,7 @@ const removeUserData = () => {
   }, [isBothVideo,checkRemoteVideoPlaying]);
 
 
-useEffect(()=>{
-  if(isBothVideo >= 20){
-    alert("Fully Connected");
-    console.log("Fully Connected");
-  }
-},[isBothVideo]);
+
 
 
 
@@ -499,16 +494,16 @@ return () => {
               playsInline
               className="absolute right-3 top-3 rounded-md  object-cover h-24 w-16 ring-1 ring-black"
             ></video>
-            <button onClick={checkRemoteVideoPlaying} className="absolute z-20 flex items-center justify-center text-center bg-blf h-14 w-14 rounded-full text-white p-1.5 right-4 bottom-24"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8 flex items-center text-center justify-center">
+            {/* <button onClick={checkRemoteVideoPlaying} className="absolute z-20 flex items-center justify-center text-center bg-blf h-14 w-14 rounded-full text-white p-1.5 right-4 bottom-24"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8 flex items-center text-center justify-center">
   <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
-</svg></button>
+</svg></button> */}
 
             <div className="flex flex-col justify-center items-center h-full sm:h-9/12 md:h-4/6 md:aspect-square">
               <video
                 ref={remoteVideoRef}
                 autoPlay
                 playsInline
-                className="w-full h-full  ring-2 ring-black bg-blm rounded-md  object-cover  "
+                className="w-full h-full md:aspect-square  ring-2 ring-black bg-blm rounded-md  object-cover  "
               ></video>
             </div>
             <div className="w-full bg-transparent  py-2 flex items-center justify-center">
