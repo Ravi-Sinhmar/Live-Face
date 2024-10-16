08257c476d0d492d0bc2ede461f1f0d88d341303
Ravi-Sinhmar <engineerfuture82@gmail.com>
2024-10-16 10:52:16 +0530

    added a reconnect button

--- src/Components/JoinMeet.js
+++ src/Components/JoinMeet.js
@@ -444,6 +444,10 @@ return () => {
               playsInline
               className="absolute right-3 top-3 rounded-md  object-cover h-24 w-16 ring-1 ring-black"
             ></video>
+            <button onClick={handleUserJoin} className="absolute bg-blf h-14 w-14 rounded-full text-white p-2 right-4 bottom-10"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+</svg></button>
+
             <div className=" flex flex-col justify-center items-center h-full">
               <video
                 ref={remoteVideoRef}
