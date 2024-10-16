d7797ea00fcf01219a4446991d210013160d5d3c
Ravi-Sinhmar <engineerfuture82@gmail.com>
2024-10-16 11:07:03 +0530

    updated design of nav buttons after click

--- src/Components/JoinMeet.js
+++ src/Components/JoinMeet.js
@@ -444,7 +444,7 @@ return () => {
               playsInline
               className="absolute right-3 top-3 rounded-md  object-cover h-24 w-16 ring-1 ring-black"
             ></video>
-            <button onClick={handleUserJoin} className="absolute bg-blf h-14 w-14 rounded-full text-white p-2 right-4 bottom-10"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8">
+            <button onClick={handleUserJoin} className="absolute text-center bg-blf h-14 w-14 rounded-full text-white p-2 right-4 bottom-36"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8 flex items-center text-center justify-center">
   <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
 </svg></button>
 
@@ -463,7 +463,7 @@ return () => {
                   className="flex flex-col text-sm items-center justify-center gap-1"
                 >
                   <svg
-                    className="size-8 p-1 rounded-full"
+                     className= {!isMicEnabled ?  "size-8 p-1 bg-blf text-blm rounded-full" : "size-8 p-1 text-blt rounded-full" }   
                     viewBox="0 0 24 24"
                     fill="none"
                     xmlns="http://www.w3.org/2000/svg"
@@ -525,7 +525,7 @@ return () => {
                     viewBox="0 0 24 24"
                     stroke-width="1.5"
                     stroke="currentColor"
-                    className="size-8 p-1 rounded-full"
+                    className= {!isVideoEnabled ?  "size-8 p-1 bg-blf text-blm rounded-full" : "size-8 p-1 text-blt rounded-full" }   
                   >
                     <path
                       stroke-linecap="round"
@@ -535,18 +535,19 @@ return () => {
                   </svg>
                   Stop
                 </button>
-                {user || admin ? <button className="z-20 w-fit px-6 text-sm font-[400] flex text-center items-center justify-center py-2 rounded-full bg-blm ring-1 ring-blt shadow-md shadow-blt h-fit" onClick={!joined && !admin ? handleUserJoin : cutCall}>{!joined && user ? "Join" : "Disconnect"}</button> : null } 
+                {user || admin ? <button className="z-20 w-fit bg-blf px-6 text-sm font-[400] flex text-center items-center justify-center py-2 rounded-full  ring-1 ring-blt shadow-md shadow-blt h-fit" onClick={!joined && !admin ? handleUserJoin : cutCall}>{!joined && user ? "Join" : "Disconnect"}</button> : null } 
                 <button
                   onClick={toggleRemoteAudio}
                   className="flex flex-col text-sm items-center justify-center gap-1"
                 >
-                  <svg
+                  <svg 
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke-width="1.5"
                     stroke="currentColor"
-                    className="size-8 p-1 rounded-full"
+                    className= {!isRemoteAudioEnabled ?  "size-8 p-1 bg-blf text-blm rounded-full" : "size-8 p-1 text-blt rounded-full" }   
+
                   >
                     <path
                       stroke-linecap="round"
