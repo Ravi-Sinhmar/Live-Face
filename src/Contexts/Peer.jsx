ee07791b21e0991cb23f8a78cc49dd52a4f43321
Ravi-Sinhmar <engineerfuture82@gmail.com>
2024-10-18 19:20:31 +0530

    twillo errror

--- src/Contexts/Peer.jsx
+++ src/Contexts/Peer.jsx
@@ -11,12 +11,9 @@ function PeerProvider(props){
       new RTCPeerConnection({
         iceServers: [
           {
-            urls: [
-              "stun:stun.l.google.com:19302",
-              "stun:global.stun.twilio.com:3478",
-            ],
-          },
-        ],
+              urls: 'stun:stun.l.google.com:19302'
+          }
+      ],
       }),
     []
   );
