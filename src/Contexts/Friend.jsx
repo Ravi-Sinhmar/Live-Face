c5c85b8c279bb3a97b265a1de5b1c0a97625a581
Ravi-Sinhmar <engineerfuture82@gmail.com>
2024-12-13 23:27:31 +0530

    updated more

--- src/Contexts/Friend.jsx
+++ src/Contexts/Friend.jsx
@@ -1,24 +1,33 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 const MyContext = React.createContext(null);
 
- export const useFriend = () =>{
-    return React.useContext(MyContext);
-}
-function FriendProvider(props){
-  const [friend,setFriend] = useState(null);
-  const [adminCon,setAdminCon] = useState("uususususususususuus");
-  const [cons,setCons] = useState({video:true,audio:true});
-  const [audioOutput,setAudioOutput] = useState("");
-  const [setting,setSetting] = useState(false);
-    return(
-        <MyContext.Provider value={{friend,setFriend,adminCon,setAdminCon,cons,setCons,setting,setSetting,audioOutput,setAudioOutput}}>
-            {props.children}
-        </MyContext.Provider>
-    )
+export const useFriend = () => {
+  return React.useContext(MyContext);
+};
+function FriendProvider(props) {
+  const [friend, setFriend] = useState(null);
+  const [adminCon, setAdminCon] = useState("uususususususususuus");
+  const [cons, setCons] = useState({ video: true, audio: true });
+  const [audioOutput, setAudioOutput] = useState("");
+  const [setting, setSetting] = useState(false);
+  return (
+    <MyContext.Provider
+      value={{
+        friend,
+        setFriend,
+        adminCon,
+        setAdminCon,
+        cons,
+        setCons,
+        setting,
+        setSetting,
+        audioOutput,
+        setAudioOutput,
+      }}
+    >
+      {props.children}
+    </MyContext.Provider>
+  );
 }
 
 export default FriendProvider;
-
-
-
- 
--- src/Contexts/Peer.jsx
+++ src/Contexts/Peer.jsx
@@ -1,15 +1,15 @@
-import React, { useMemo,useEffect,useState, useCallback } from "react";
+import React, { useMemo, useEffect, useState, useCallback } from "react";
 const PeerContext = React.createContext(null);
-export const usePeer = () =>{
+export const usePeer = () => {
   return React.useContext(PeerContext);
-}
+};
 // final
 
-function PeerProvider(props){
-  const [remoteStream,setRemoteStream] = useState();
-  const [adminConnection,setAdminConnection] = useState(null);
-  const [userConnection,setUserConnection] = useState(null);
-  
+function PeerProvider(props) {
+  const [remoteStream, setRemoteStream] = useState();
+  const [adminConnection, setAdminConnection] = useState(null);
+  const [userConnection, setUserConnection] = useState(null);
+
   const peer = useMemo(
     () =>
       new RTCPeerConnection({
@@ -19,72 +19,79 @@ function PeerProvider(props){
               "stun:stun.l.google.com:19302",
               "stun:global.stun.twilio.com:3478",
             ],
-          }
-      ],
+          },
+        ],
       }),
     []
   );
 
   // create offer
-  const createOffer = async()=>{
+  const createOffer = async () => {
     const offer = await peer.createOffer();
     await peer.setLocalDescription(offer);
     return peer.localDescription;
   };
 
-  const createAnswer  = async (offer)=>{
-  setAdminConnection(peer.connectionState);
-   await peer.setRemoteDescription(offer);
-const answer = await peer.createAnswer();
-await peer.setLocalDescription(answer);
-return peer.localDescription;
+  const createAnswer = async (offer) => {
+    setAdminConnection(peer.connectionState);
+    await peer.setRemoteDescription(offer);
+    const answer = await peer.createAnswer();
+    await peer.setLocalDescription(answer);
+    return peer.localDescription;
   };
 
-  const setRemoteAnswer = async(answer)=>{
-  setUserConnection(peer.connectionState);
- await peer.setRemoteDescription(answer);
-return true;
+  const setRemoteAnswer = async (answer) => {
+    setUserConnection(peer.connectionState);
+    await peer.setRemoteDescription(answer);
+    return true;
   };
 
+  // sending Video
+  const sendVideo = async (video) => {
+    const tracks = await video.getTracks();
+    for (const track of tracks) {
+      peer.addTrack(track, video);
+    }
+  };
 
-// sending Video
-const sendVideo = async (video)=>{
-  const tracks = await video.getTracks();
-  for(const track of tracks){
-    peer.addTrack(track,video);
-  }
-}
+  const handleSendVideo = useCallback(async (event) => {
+    const video = await event.streams[0];
+    console.log("GOT TRACKS!!", video[0]);
+    setRemoteStream(video);
+  }, []);
 
-const handleSendVideo = useCallback(async(event)=>{
-  const video = await event.streams[0];
-  console.log("GOT TRACKS!!",video[0]);
-  setRemoteStream(video);
-},[]);
+  useEffect(() => {
+    peer.addEventListener("track", handleSendVideo);
+    return () => {
+      peer.removeEventListener("track", handleSendVideo);
+    };
+  }, [peer, handleSendVideo, userConnection, adminConnection]);
 
-
-
-useEffect(()=>{
-  peer.addEventListener('track',handleSendVideo);
-  return ()=>{
-    peer.removeEventListener('track',handleSendVideo);
-  }
-},[peer,handleSendVideo,userConnection,adminConnection]);
-
-   // close connection
-   const disconnect = useCallback(() => {
+  // close connection
+  const disconnect = useCallback(() => {
     if (peer.connectionState !== "closed") {
       peer.close();
       setRemoteStream(null); // Optionally, reset other relevant state variables (setting, cons)
-  }
-}, [peer]);
-
+    }
+  }, [peer]);
 
   return (
-    <PeerContext.Provider value={{ peer ,disconnect, createOffer,createAnswer,setRemoteAnswer,sendVideo,remoteStream,adminConnection,userConnection}}>
+    <PeerContext.Provider
+      value={{
+        peer,
+        disconnect,
+        createOffer,
+        createAnswer,
+        setRemoteAnswer,
+        sendVideo,
+        remoteStream,
+        adminConnection,
+        userConnection,
+      }}
+    >
       {props.children}
     </PeerContext.Provider>
   );
-};
-
+}
 
 export default PeerProvider;
